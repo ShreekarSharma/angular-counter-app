47136643c88485587796f867bb1e7ff90f925753
Shreekar Sharma <shreekar@mail.com>
2024-01-25 20:22:12 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'counter-app';
+  title:string = 'Counter App';
+  count:number = 0;
+  handleIncrease = () => {
+    this.count = this.count + 1;
+  };
+  handleDecrease = () => {
+    if(this.count > 0){
+      this.count = this.count - 1;
+    } else{
+      this.count = 0;
+    }
+  };
+  handleReset = () => {
+    this.count = 0;
+  };
 }
